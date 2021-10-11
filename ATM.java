e0df85c40b46240c796dd2dd8281efab39d334db
AKHILESH-TRIPATHI03 <akhilesh03tripathi@gmail.com>
2021-10-11 16:57:54 +0530

    atm commit

---
+++ ATM.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+
+public class ATM extends Option{
+public static void main(String[] args) throws IOException {
+    Option option = new Option();
+    option.getLogin();
+    
+}
+    
+}
